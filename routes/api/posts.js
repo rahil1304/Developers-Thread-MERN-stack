4d1ef430623271883668ae486530b017b2d507e6
Rahil Sarvaiya <32304956+rahil1304@users.noreply.github.com>
2020-04-06 23:45:51 +0530

    get and delete post routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -41,5 +41,72 @@ router.post('/', [ auth, [check('text','Text is required')
      
 );
 
+//@route        GET api/posts
+//@description  Get all posts
+//@acess        Private (could be Private/Public)
+router.get('/',auth, async (req,res)=> {
+    try {
+        const posts= await Post.find().sort({date:-1});
+        res.json(posts);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+
+});
+
+//@route        GET api/posts/:id
+//@description  Get  post by id
+//@acess        Private (could be Private/Public)
+router.get('/:id',auth, async (req,res)=> {
+    try {
+        const post= await Post.findById(req.params.id);
+        
+        if(!req.params.id.match(/^[0-9a-fA-F]{24}$/) || !post){
+            return res.status(404).json({msg:'Post not found'});
+        }
+
+        res.json(post);
+    } catch (err) {
+        console.error(err.message);
+        if(err.kind === 'ObjectId' || !req.params.id.match(/^[0-9a-fA-F]{24}$/)){
+            return res.status(404).json({msg:'Post not found'});
+        }
+        res.status(500).send('Server Error');
+    }
+
+});
+
+
+//@route        DELETE api/posts/:id
+//@description  Delete a post
+//@acess        Private (could be Private/Public)
+router.delete('/:id',auth, async (req,res)=> {
+    try {
+        const post= await Post.findById(req.params.id);
+        
+        if(!req.params.id.match(/^[0-9a-fA-F]{24}$/) || !post){
+            return res.status(404).json({msg:'Post not found'});
+        }
+
+        //Check user
+        if(post.user.toString() !== req.user.id){
+            return res.status(401).json({msg: 'User not authorized'});
+        }
+        await post.remove();
+
+        res.json({msg:'Post removed'});
+    } catch (err) {
+        console.error(err.message);
+        
+        if(err.kind === 'ObjectId' || !req.params.id.match(/^[0-9a-fA-F]{24}$/)){
+            return res.status(404).json({msg:'Post not found'});
+        }
+
+        res.status(500).send('Server Error');
+    }
+
+});
+
 
 module.exports=router;
