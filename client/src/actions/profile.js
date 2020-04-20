72b22e16cbb5ab0eb60d2164f663f01eb55844b8
Rahil Sarvaiya <32304956+rahil1304@users.noreply.github.com>
2020-04-20 18:58:05 +0530

    VIEW PROFILE ERROR

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -48,7 +48,7 @@ export const getProfiles = () => async (dispatch) => {
 //Get profile by id
 export const getProfileById = (userId) => async (dispatch) => {
   try {
-    const res = await axios.get(`api/profile/user/${userId}`);
+    const res = await axios.get(`/api/profile/user/${userId}`);
     dispatch({
       type: GET_PROFILE,
       payload: res.data,
