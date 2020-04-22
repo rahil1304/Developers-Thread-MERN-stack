41d192fa7c243d514844bb90215ed653971914d5
Rahil Sarvaiya <32304956+rahil1304@users.noreply.github.com>
2020-04-22 18:15:28 +0530

    deleting posts

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,4 +1,9 @@
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from "../actions/types";
+import {
+  GET_POSTS,
+  POST_ERROR,
+  UPDATE_LIKES,
+  DELETE_POST,
+} from "../actions/types";
 
 const intitialState = {
   posts: [],
@@ -17,7 +22,12 @@ export default function (state = intitialState, action) {
         posts: payload,
         loading: false,
       };
-
+    case DELETE_POST:
+      return {
+        ...state,
+        posts: state.posts.filter((post) => post._id !== payload),
+        loading: false,
+      };
     case POST_ERROR:
       return {
         ...state,
@@ -32,6 +42,7 @@ export default function (state = intitialState, action) {
         ),
         loading: false,
       };
+
     default:
       return state;
   }
